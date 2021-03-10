dd69eab85637ef04dc17ef9e1d1137c501df9b4c
leribeir0s <leandro.souza@easynvest.com.br>
2021-03-10 10:59:05 -0300

    pagina dinamica ambiente teste

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,11 +1,7 @@
-import Link from 'next/link'
-
 function Sobre() {
     return (<div>
         <h1>Sobre</h1>
-        <Link href="/">
-            <a>Acessar página Home</a>
-        </Link>
+        <a href="/">Acessar página Home</a>
     </div>)
 }
 
