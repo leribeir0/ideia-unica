bd2adf09e4e31ee13b05b5ca9ec224013234374a
leribeir0s <leandro.souza@easynvest.com.br>
2021-03-10 10:46:09 -0300

    pagina dinamica preprod

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,8 @@
-import { useState } from 'react';
-
-function Home(){
-    return (
-        <div>
-            <h2>Home</h2> 
-            <Contador/>
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador +1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+function Home() {
+    return (<div>
+        <h1>Home</h1>
+        <a href="/sobre">Acessar página Sobre</a>
+    </div>)
 }
 
 export default Home
