5f7d05d1451639abb94c2e35c336266ecbd0e748
leribeir0s <leandro.souza@easynvest.com.br>
2021-03-11 11:22:02 -0300

    add cabecalho stale while revalidate

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,10 +1,12 @@
 async function tempo(request, response) {
     const apiSecret = process.env.MTA_API_SECRET;
     const dynamicDate = new Date();
-    //new
+    
     const currentTimeResponse = await fetch(`https://bustime.mta.info/api/where/stops-for-location.json?lat=40.748433&lon=-73.985656&latSpan=0.005&lonSpan=0.005&key=${apiSecret}`);
     const currentTimeResponseJson = await currentTimeResponse.json();
     const currentTime = currentTimeResponseJson.currentTime;
+
+    response.setHeader('Cache-Control','s-maxage=10, stale-while-revalidate')
 
     response.json({
         date: dynamicDate.toGMTString(),
