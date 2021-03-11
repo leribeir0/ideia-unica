c40e0ae1417ee5c47c4430c8ee58608e8db93d51
leribeir0s <leandro.souza@easynvest.com.br>
2021-03-11 11:00:47 -0300

    consulta api com secret

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,14 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+    const apiSecret = process.env.MTA_API_SECRET;
     const dynamicDate = new Date();
 
+    const currentTimeResponse = await fetch(`https://bustime.mta.info/api/where/stops-for-location.json?lat=40.748433&lon=-73.985656&latSpan=0.005&lonSpan=0.005&key=${apiSecret}`);
+    const currentTimeResponseJson = await currentTimeResponse.json();
+    const currentTime = currentTimeResponseJson.currentTime;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        currentTime: currentTime
     });
 }
 
